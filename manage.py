94e928f068dcac04f176c366684dd7ba4d0aa539
Ethan Steininger <esteininger21@gmail.com>
2020-11-05 16:39:05 -0500

    moved files around

--- manage.py
+++ manage.py
@@ -15,5 +15,9 @@ app_config = {
 def home():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
